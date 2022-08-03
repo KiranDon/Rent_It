7d32a353065065701eaab369650562e9bcdac5b3
KiranDon <ushakiranmelasthri123@gmail.com>
2022-08-03 16:31:02 +0530

    Convert complete Code into ejs

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const ejs = require("ejs")
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+app.get("/allProducts", function(req, res){
+    res.render("allProducts");
+})
+app.get("/about", function(req, res){
+    res.render("about");
+})
+app.get("/contact", function(req, res){
+    res.render("contact");
+})
+app.get("/login", function(req, res){
+    res.render("login");
+})
+app.get("/register", function(req, res){
+    res.render("register");
+})
+app.get("/product", function(req, res){
+    res.render("product");
+})
+app.get("/checkOut", function(req, res){
+    res.render("checkOut");
+})
+
+app.listen(3333, function(){
+    console.log("server started at http://localhost:3333");
+})
